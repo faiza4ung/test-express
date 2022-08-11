be99060e12979607f50c7f6d9c1288853b8b3ec6
Ungguh Faizaturrohman <otemoos@outlook.com>
2022-08-11 15:29:18 +0700

    router param

--- src/routes/tourRoute.js
+++ src/routes/tourRoute.js
@@ -9,6 +9,11 @@ const {
 
 const router = express.Router();
 
+router.param("id", (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+  next();
+});
+
 router.route("/").get(getAllTours).post(createTours);
 router.route("/:id").get(getTours).patch(updateTours).delete(deleteTours);
 
