b8404e035eaf5db4ecaabb408ee788b46e4aeda2
Ungguh Faizaturrohman <otemoos@outlook.com>
2022-08-11 08:59:13 +0700

    express.json() add

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res
     .status(200)
     .json({ message: "Hello from server", app: "Kendalisada Tour" });
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.status(201).json({ message: "POST METHOD" });
 });
 
 const port = 3000;
