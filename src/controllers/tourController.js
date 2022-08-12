b332ea46b13655eb5817b395a874264141cd43e2
Ungguh Faizaturrohman <otemoos@outlook.com>
2022-08-12 11:01:09 +0700

    pagination

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -41,8 +41,22 @@ exports.getAllTours = async (req, res) => {
       query = query.select("-__v");
     }
 
+    //** 4) Pagination */
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 5;
+    const skip = (page - 1) * limit;
+
+    //? page=2&limit=5, 1-5, page 1, 6-10, page 2, dst
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("This page does not exist");
+    }
+
     //** EXECUTE QUERY */
     const tours = await query;
+    //? query.sort().select().skip().limit()
 
     //** SEND RESPONSE */
     res.status(200).json({
@@ -56,7 +70,7 @@ exports.getAllTours = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: "failed",
-      message: err,
+      message: err.message,
     });
   }
 };
