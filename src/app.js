c4708242c9a80be4e688fa9c6de15be21b44cd6e
Ungguh Faizaturrohman <otemoos@outlook.com>
2022-08-12 19:03:35 +0700

    class AppError

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express"),
-  morgan = require("morgan"),
-  dotenv = require("dotenv");
+  AppError = require("./utils/appError"),
+  globalErrorHandler = require("./controllers/errorController");
+(morgan = require("morgan")), (dotenv = require("dotenv"));
 
 const tourRoute = require("./routes/tourRoute"),
   userRoute = require("./routes/userRoute");
@@ -25,21 +26,10 @@ app.use("/api/v1/users", userRoute);
 
 //** MIDDLEWARE ROUTE UNREGISTERED */
 app.all("*", (req, res, next) => {
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = "failed";
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 //** ERROR HANDLING */
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
