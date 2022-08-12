592622bd2f568202ef76f8d5fda4b8368c6229f9
Ungguh Faizaturrohman <otemoos@outlook.com>
2022-08-12 18:47:24 +0700

    error handler

--- src/app.js
+++ src/app.js
@@ -25,9 +25,20 @@ app.use("/api/v1/users", userRoute);
 
 //** MIDDLEWARE ROUTE UNREGISTERED */
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "failed",
-    message: `Can't find ${req.originalUrl} on this server`,
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = "failed";
+  err.statusCode = 404;
+  next(err);
+});
+
+//** ERROR HANDLING */
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
