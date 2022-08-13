29b449868cd38125093d28cf4466e2eec8918494
Ungguh Faizaturrohman <otemoos@outlook.com>
2022-08-13 14:58:39 +0700

    signup

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,4 +2,6 @@ module.exports = {
   MONGO_URL:
     process.env.MONGO_URL || "mongodb://127.0.0.1:27017/kendalisada-tour",
   PORT: process.env.PORT || "3001",
+  JWT_KEY: process.env.JWT_KEY || "Rahasia-banget-jWT*token",
+  JWT_EXPIRE: process.env.JWT_EXPIRE, //? gimana cara ubah jadi string expiresIn
 };
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModel"),
   catchAsync = require("../utils/catchAsync"),
-  {} = require("jsonwebtoken");
+  { JWT_KEY } = require("../configs"),
+  { sign } = require("jsonwebtoken");
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -9,8 +10,15 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
+
+  const token = sign({ id: newUser._id }, JWT_KEY, {
+    // expiresIn: JWT_EXPIRE,
+    expiresIn: '25m',
+  });
+
   res.status(201).json({
     status: "success",
+    token,
     data: { user: newUser },
   });
 });
